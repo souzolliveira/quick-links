c159bdd8ef3bf1d0fa6f671b36e2c5f1efc5b30d
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2020-12-22 23:56:20 -0300

    green: creating App component and adding on project structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./views/App/App";
 
 ReactDOM.render(
-  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/views/App/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./App.module.scss";
+
+const App = () => {
+  return <div id="app" className={styles.app}></div>;
+};
+
+export default App;
