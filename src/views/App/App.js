2314da79dd4355223e4db8695750a2f6415d772d
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2020-12-23 19:51:53 -0300

    green: adding sidebar and mainbar

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -3,7 +3,16 @@ import React from "react";
 import styles from "./App.module.scss";
 
 const App = () => {
-  return <div id="app" className={styles.app}></div>;
+  return (
+    <div id="app" className={styles.app}>
+      <div id="sidebar" className={styles.sidebar}>
+        <h4>Quick Links</h4>
+      </div>
+      <div id="mainbar" className={styles.mainbar}>
+        <h2>Files</h2>
+      </div>
+    </div>
+  );
 };
 
 export default App;
