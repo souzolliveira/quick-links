6584d63c99829db445bcec22b8c925c141cf38d4
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2020-12-23 20:30:50 -0300

    red: verify if when click on Files title, the application redirects to root

--- cypress/integration/files.spec.js
+++ cypress/integration/files.spec.js
@@ -8,4 +8,9 @@ describe("Files", () => {
   it("Verify if files are displayed", () => {
     cy.get("#mainbar").find("a");
   });
+
+  it("Verify if when you click on Files title, the application redirects to root", () => {
+    cy.get("#filesTitle").click();
+    cy.url().should("eq", baseUrl);
+  });
 });
