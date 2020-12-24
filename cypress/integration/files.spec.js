b71895e32d126acf869120dfffd5b593623a1ae6
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2020-12-23 21:30:41 -0300

    red: adding test to verify if navigation to file is correct

--- cypress/integration/files.spec.js
+++ cypress/integration/files.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable jest/valid-expect-in-promise */
+/* eslint-disable no-undef */
 const { baseUrl } = Cypress.config();
 
 describe("Files", () => {
@@ -11,6 +13,21 @@ describe("Files", () => {
 
   it("Verify if when you click on Files title, the application redirects to root", () => {
     cy.get("#filesTitle").click();
-    cy.url().should("eq", baseUrl);
+    cy.url().should("eq", `${baseUrl}/`);
+  });
+
+  it("Verify if when you click on a file, the application redirects to the file", () => {
+    cy.get("#mainbar")
+      .find("a")
+      .then((data) => {
+        const index = Math.floor(Math.random() * data.length);
+        const file = data[index].innerHTML;
+        cy.get(`#${file}`)
+          .click()
+          .then(() => {
+            cy.url().should("eq", `${baseUrl}/${file}`);
+            cy.get(`#content-${file}`).should("exist");
+          });
+      });
   });
 });
