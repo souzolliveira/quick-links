85607934d1b178db929d9c832453fbb65e4a9fec
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2020-12-24 19:44:05 -0300

    red: adding verification to appear the last clicked link in first position on quick links

--- cypress/integration/quicklinks.spec.js
+++ cypress/integration/quicklinks.spec.js
@@ -21,4 +21,33 @@ describe("Files", () => {
       .find("a")
       .then((data) => expect(data.length).to.equal(1));
   });
+
+  it("Verify if when click on file, it's added on quick links in first position", () => {
+    let file01, file02;
+    cy.get("#mainbar")
+      .find("a")
+      .then((data) => {
+        const index = Math.floor(Math.random() * data.length);
+        file01 = data[index].innerHTML;
+        cy.get(`#${file01}`).click();
+      });
+    cy.get("#filesTitle").click();
+    cy.get("#mainbar")
+      .find("a")
+      .then((data) => {
+        const index = Math.floor(Math.random() * data.length);
+        file02 = data[index].innerHTML;
+        if (file02 === file01) {
+          if (file01 === data[0].innerHTML) {
+            file02 = data[index + 1].innerHTML;
+          } else {
+            file02 = data[index - 1].innerHTML;
+          }
+        }
+        cy.get(`#${file02}`).click();
+      });
+    cy.get("#sidebar")
+      .find("a")
+      .then((data) => expect(data[0].innerHTML).to.equal(file02));
+  });
 });
