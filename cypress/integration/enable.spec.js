9630269bb052f4b1cbeaded90b498565b4da71b3
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2020-12-24 22:49:42 -0300

    red: verify if enable function works

--- cypress/integration/enable.spec.js
+++ cypress/integration/enable.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/valid-expect */
 /* eslint-disable jest/valid-expect-in-promise */
 /* eslint-disable no-undef */
 const { baseUrl } = Cypress.config();
@@ -9,5 +10,30 @@ describe("Enalbe quick links", () => {
 
   it("Verify if checkbox to enable is displayed", () => {
     cy.get("#enableCheckbox").should("exist");
+  });
+
+  it("Verify if enable checkbox works", () => {
+    cy.get("#enableCheckbox").uncheck();
+    let file01, file02;
+    let index01, index02;
+    cy.get("#mainbar")
+      .find("a")
+      .then((data) => {
+        index01 = Math.floor(Math.random() * data.length);
+        file01 = data[index01].innerHTML;
+        cy.get(`#${file01}`).click();
+        cy.get("#filesTitle").click();
+      });
+    cy.get("#sidebar").find("a").should("not.exist");
+
+    cy.get("#enableCheckbox").check();
+    cy.get("#mainbar")
+      .find("a")
+      .then((data) => {
+        index02 = Math.floor(Math.random() * data.length);
+        file02 = data[index02].innerHTML;
+        cy.get(`#${file02}`).click();
+      });
+    cy.get("#sidebar").find("a").should("exist");
   });
 });
