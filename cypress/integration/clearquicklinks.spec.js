51dffb502cef243bb2bb1ddb27d556fa7a0e5f1d
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2020-12-24 22:13:29 -0300

    red: verify if clear button works

--- cypress/integration/clearquicklinks.spec.js
+++ cypress/integration/clearquicklinks.spec.js
@@ -10,4 +10,27 @@ describe("Clear quick links", () => {
   it("Verify if clear button is displayed", () => {
     cy.get("#clearButton").should("exist");
   });
+
+  it("Verify if clear button works", () => {
+    let file01, file02;
+    let index01, index02;
+    cy.get("#mainbar")
+      .find("a")
+      .then((data) => {
+        index01 = Math.floor(Math.random() * data.length);
+        file01 = data[index01].innerHTML;
+        do {
+          index02 = Math.floor(Math.random() * data.length);
+        } while (index02 === index01);
+        file02 = data[index02].innerHTML;
+        cy.get(`#${file01}`).click();
+        cy.get("#filesTitle").click();
+        cy.get(`#${file02}`).click();
+      });
+    cy.get("#clearButton")
+      .click()
+      .then(() => {
+        cy.get("#sidebar").find("a").should("not.exist");
+      });
+  });
 });
